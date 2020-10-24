36782024451bf6fbd2f55aede899e5da64fae12f
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2020-10-24 21:51:57 +0200

    add copyright notice

--- Benchmarks/ORootsBench.java
+++ Benchmarks/ORootsBench.java
@@ -1,3 +1,10 @@
+/**
+* Copyright Johannes Kloimböck 2020.
+* Distributed under the Boost Software License, Version 1.0.
+* (See accompanying file LICENSE or copy at
+* https://www.boost.org/LICENSE_1_0.txt)
+*/
+
 import oroots.*;
 import java.util.Scanner;
 
