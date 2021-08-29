7b35e621032273ae5ce24f5ea47fe25bfffab434
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2021-08-29 18:00:41 +0200

    add rcbrt functions
    these compute 1 / cbrt

--- C/ocbrt.c
+++ C/ocbrt.c
@@ -8,6 +8,62 @@
 #include "oroots.h"
 #include <stdbool.h>
 
+double orcbrt(const double a) {
+#if CHECK_SPECIAL_CASES != 0
+	if (a == 0.0) {
+		return DOUBLE_INF;
+	}
+#endif
+
+	double_ull val;
+	val.d = a;
+
+	unsigned long long sign = val.ull & 0x8000000000000000ULL;
+
+	val.ull ^= sign;
+
+	int exponent = (int)(val.ull >> 52);
+
+#if SUBNORMAL_NUMBERS != 0
+	bool is_sub_normal = !exponent;
+#endif
+
+	exponent = 4092 - exponent;
+	exponent /= 3;
+
+#if SUBNORMAL_NUMBERS != 0
+	if (is_sub_normal) {
+		if (val.ull < 3) {
+			double_ull r;
+			r.ull = val.ull == 1ULL ? 0x2990000000000000ULL : 0x299428a2f98d728bULL;
+			r.ull |= sign;
+			return r.d;
+		}
+
+		int sub_normal_exponent = leading_zeros_ull(&val.ull);
+		sub_normal_exponent /= 3;
+		exponent += sub_normal_exponent;
+	}
+#endif
+
+	double_ull guess;
+	guess.ull = (unsigned long long)(exponent) << 52;
+
+	double thirdA = a * ONE_THIRD;
+
+	for (int i = 0; i < 6; ++i) {
+		guess.d *= (FOUR_THIRDS - (thirdA * guess.d) * (guess.d * guess.d));
+	}
+
+	corr_t g = (corr_t)(guess.d);
+	g *= ((corr_t)(FOUR_THIRDS) - ((corr_t)(a) * ONE_THIRD) * (g * g) * g);
+	guess.d = (double)(g);
+
+	guess.ull |= sign;
+
+	return guess.d;
+}
+
 double ocbrt(const double a) {
 #if CHECK_SPECIAL_CASES != 0
 	if (a == 0.0) {
@@ -20,7 +76,7 @@ double ocbrt(const double a) {
 
 	unsigned long long sign = val.ull & 0x8000000000000000ULL;
 
-	val.ull &= 0x7fffffffffffffffULL;
+	val.ull ^= sign;
 
 	int exponent = (int)(val.ull >> 52);
 
@@ -39,25 +95,79 @@ double ocbrt(const double a) {
 	}
 #endif
 
-	val.ull |= sign;
-
 	double_ull guess;
 	guess.ull = (unsigned long long)(exponent) << 52;
-
-	guess.ull |= sign;
 
 	for (int i = 0; i < 5; ++i) {
 		guess.d = (2.0 * guess.d + (val.d / (guess.d * guess.d))) * ONE_THIRD;
 	}
 
-	corr_t diff = ((corr_t)(guess.d) * (corr_t)guess.d * (corr_t)(guess.d)) - (corr_t)val.d;
-	diff /= 3.0 * (corr_t)(guess.d) * (corr_t)(guess.d);
+	corr_t guess_sqr = (corr_t)(guess.d) * (corr_t)(guess.d);
+	corr_t diff = (guess_sqr * (corr_t)(guess.d)) - (corr_t)(val.d);
+	diff /= (corr_t)(3.0) * guess_sqr;
 	guess.d -= (double)(diff);
+	guess.ull |= sign;
 
 	return guess.d;
 }
 
-extern float ocbrtf(const float a) {
+float orcbrtf(const float a) {
+#if CHECK_SPECIAL_CASES != 0
+	if (a == 0.0) {
+		return FLOAT_INF;
+	}
+#endif
+
+	float_ul val;
+	val.f = a;
+
+	unsigned long sign = val.ul & 0x80000000UL;
+
+	val.ul ^= sign;
+
+	int exponent = (int)(val.ul >> 23);
+
+#if SUBNORMAL_NUMBERS != 0
+	bool is_sub_normal = !exponent;
+#endif
+
+	exponent = 508 - exponent;
+	exponent /= 3;
+
+#if SUBNORMAL_NUMBERS != 0
+	if (is_sub_normal) {
+		if (val.ul < 3) {
+			float_ul r;
+			r.ul = val.ul == 1UL ? 0x26a14518UL : 0x26cb2ff5UL;
+			r.ul |= sign;
+			return r.f;
+		}
+
+		int sub_normal_exponent = leading_zeros_ul(&val.ul);
+		sub_normal_exponent /= 3;
+		exponent += sub_normal_exponent;
+	}
+#endif
+
+	float_ul guess;
+	guess.ul = (unsigned long)(exponent) << 23;
+
+	float thirdA = a * (float)(ONE_THIRD);
+
+	for (int i = 0; i < 5; ++i) {
+		guess.f *= ((float)(FOUR_THIRDS) - thirdA * guess.f * guess.f * guess.f);
+	}
+
+	corrf_t g = (corrf_t)(guess.f);
+	g *= ((corrf_t)(FOUR_THIRDS)-((corrf_t)(a) * ONE_THIRD) * (g * g) * g);
+	guess.f = (float)(g);
+
+	guess.ul |= sign;
+
+	return guess.f;
+}
+
+float ocbrtf(const float a) {
 #if CHECK_SPECIAL_CASES != 0
 	if (a == 0.0f) {
 		return a;
@@ -88,20 +198,18 @@ extern float ocbrtf(const float a) {
 	}
 #endif
 
-	val.ul |= sign;
-
 	float_ul guess;
 	guess.ul = (unsigned long)(exponent) << 23;
-
-	guess.ul |= sign;
 
 	for (int i = 0; i < 4; ++i) {
 		guess.f = (2.0f * guess.f + (val.f / (guess.f * guess.f))) * (float)(ONE_THIRD);
 	}
 
-	corrf_t diff = ((corrf_t)(guess.f) * (corrf_t)guess.f * (corrf_t)(guess.f)) - (corrf_t)val.f;
-	diff /= 3.0f * (corrf_t)(guess.f) * (corrf_t)(guess.f);
+	corrf_t guess_sqr = (corrf_t)(guess.f) * (corrf_t)(guess.f);
+	corrf_t diff = (guess_sqr * (corrf_t)(guess.f)) - (corrf_t)(val.f);
+	diff /= (corrf_t)(3.0) * guess_sqr;
 	guess.f -= (float)(diff);
+	guess.ul |= sign;
 
 	return guess.f;
 }
