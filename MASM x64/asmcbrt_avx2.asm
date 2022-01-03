435d29af09a312a71914398ced11dac92c49700f
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2022-01-03 12:19:54 +0100

    update Copyright notices

--- MASM x64/asmcbrt_avx2.asm
+++ MASM x64/asmcbrt_avx2.asm
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The MIT License (MIT)                                                                         ;;
 ;;                                                                                               ;;
-;; Copyright © 2021 Johannes Kloimböck                                                           ;;
+;; Copyright © 2021 - 2022 Johannes Kloimböck                                                    ;;
 ;;                                                                                               ;;
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy of this software ;;
 ;; and associated documentation files (the “Software”), to deal in the Software without          ;;
